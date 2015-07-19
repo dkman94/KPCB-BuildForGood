809b1b2c297eda7fba7738abadc6162224fc8a3a
Deepak Kumar <deepak@emjoyment.com>
2015-07-19 15:46:49 -0700

    added maps..hopefully?

--- app/src/main/java/app/android/kpcbmyanmarmapper/HomeActivity.java
+++ app/src/main/java/app/android/kpcbmyanmarmapper/HomeActivity.java
@@ -1,17 +1,30 @@
 package app.android.kpcbmyanmarmapper;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HomeActivity extends ActionBarActivity {
+
+public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -32,5 +45,12 @@ public class HomeActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
